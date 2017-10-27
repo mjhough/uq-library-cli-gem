51645b9839888f26b323eced6cc3bf7604cb410b
Matt Hough <matt@hough.tv>
2017-10-28 08:55:41 +1000

    Build out CLI interface

--- lib/uq_libraries/cli.rb
+++ lib/uq_libraries/cli.rb
@@ -1,22 +1,32 @@
 class UqLibraries::CLI
 
+    def call
+        welcome # Welcomes the user
+
+        list_libraries # Iterates through all libraries and lists them with an index
+
+        user_input # Takes the user input and calls methods depending on input
+    end
+
     def welcome
         puts "Welcome to the UQ Library Gem!"
         puts ""
     end
 
-    def list_libraries
-        puts <<-DOC.gsub /^\s*/, ''
-            1. Architecture & Music Library
-            2. Biological Sicences Library
-            3. D.H. Engineering & Sciences Library
-            4. Duhig Library
-            ...
+    def list_libraries # Gets libraries from array and puts out their names preceded by their index starting at 1.
+        UqLibraries::Library.all.each.with_index(1) do |library, index|
+            puts "#{index}. #{library.name}"
+        end
 
-        DOC
+        # Example Output
+            # 1. Architecture & Music Library
+            # 2. Biological Sicences Library
+            # 3. D.H. Engineering & Sciences Library
+            # 4. Duhig Library
+            # ...
     end
 
-    def user_input
+    def user_input # Takes the user input and calls methods depending on input
         input = nil
         while input != "exit"
             puts "\nSelect a library using its index number for more information"
@@ -24,22 +34,19 @@ class UqLibraries::CLI
             print "> "
             input = gets.strip.downcase
 
-            case input
-                when "1"
-                    puts "More information on deal 1"
-                when "2"
-                    puts "More information on deal 1"
-                when "3"
-                    puts "More information on deal 1"
-                when "4"
-                    puts "More information on deal 1"
-                when "list"
-                    list_libraries
-                else
-                    puts "Whoa, baby! That's not a library."
+            if input == 1..12
+                more_info(input)
+            elsif input == "list"
+                list_libraries
+            else
+                puts "That's not a library! Enter the index number of a library (1-9) or type 'list' for a list of libraries"
             end
         end
         goodbye
+    end
+
+    def more_info(input) # Displays more information on the library using the input provided by the user (busy status for each level)
+        UqLibraries::Library.all[input].info
     end
 
     def goodbye
--- lib/uq_libraries/scraper.rb
+++ lib/uq_libraries/scraper.rb
@@ -1,5 +1,12 @@
 class UqLibraries::Scraper
 
+
+
+    # Scrape https://www.library.uq.edu.au/uqlsm/availablepcsembed.php
+    # Return the scraped website
 end
 
-https://www.library.uq.edu.au/uqlsm/availablepcsembed.php
+
+
+name = "a href" text
+time = ".uqtext" text
