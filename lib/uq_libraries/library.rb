c148bbb0bf257bd8cbd43609bb66658840ecc244
Matt Hough <matt@hough.tv>
2017-10-28 22:58:28 +1000

    Create library#initialize method. Remove need for arguments in UqLibraries::Scraper#scrape_main_page.

--- lib/uq_libraries/library.rb
+++ lib/uq_libraries/library.rb
@@ -1,3 +1,15 @@
 class UqLibraries::Library
-    
+    attr_accessor :name, :url, :total_available, :out_of_available
+
+    def initialize(library_hash)
+        library_hash.each {|attribute, value| self.send("#{attribute}=", value)}
+        @@all << self
+    end
 end
+
+
+# Initialize a library from the libraries and add itself to @all
+    # name
+    # url
+    # total_avilable
+    # out_of_available
--- lib/uq_libraries/scraper.rb
+++ lib/uq_libraries/scraper.rb
@@ -5,10 +5,10 @@ class UqLibraries::Scraper
     @main_url = "#{@base_url}#{@main_page}"
     
 
-    def self.scrape_main_page(main_url)
+    def self.scrape_main_page
         libraries = []
-        frontpage = Nokogiri::HTML(open(main_url))
-        
+        frontpage = Nokogiri::HTML(open(@main_url))
+
         frontpage.css(".chart tr").each do |row|
             name = row.css("a[href]").text
 
@@ -20,44 +20,50 @@ class UqLibraries::Scraper
 
             libraries << {name: name, total_available: total_available, out_of_available: out_of_available, library_url: library_url}
         end
+        libraries
     end
 
     def self.scrape_details_page(library_url)
-        library_levels = []
         details_page = Nokogiri::HTML(open(library_url))
 
         library_name = details_page.css("h3")
         test = details_page.css("table tr").collect do |level|
             case level.css("a[href]").text.downcase
                 when "level 1"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
                 when "level 2"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
                 when "level 3"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
                 when "level 4"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
                 when "level 5"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
                 when "level 6"
+                    name = level.css("a[href]").text
                     total_available = level.css(".right").text.split(" ")[0]
                     out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {total_available: total_available, out_of_available: out_of_available}
-                else
-                    puts "ERROR: #{level.css("a[href]").text.downcase}"
-                    raise "There was an issue. Please submit an issue on GitHub: https://github.com/Tolenno/uq-library-cli-gem with a screenshot of the error".colorize(:red)
+                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
+                # else
+                #     puts "ERROR: #{level.css("a[href]").text.downcase}"
+                #     raise "There was an issue. Please submit an issue on GitHub: https://github.com/Tolenno/uq-library-cli-gem with a screenshot of the error".colorize(:red)
             end
-        end
+        end.compact
     end
 end
 
@@ -73,3 +79,5 @@ end
 # }
 
 # UqLibraries::Scraper.scrape_details_page("https://www.library.uq.edu.au/uqlsm/availablepcsembed.php?branch=BSL")
+
+# UqLibraries::Scraper.scrape_details_page(library_url)
