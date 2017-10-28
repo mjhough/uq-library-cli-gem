781ad3584b3c8f8e2844d246e35af9ae78427799
Matt Hough <matt@hough.tv>
2017-10-28 23:16:57 +1000

    Shorten code in Uqlibrary::Scraper#scrape_details_page method and fix nil return error where tr tags were iterated over too many times. See StackOverflow question: https://stackoverflow.com/questions/46989787/nokogiri-iterating-over-tr-tags-too-many-times.

--- lib/uq_libraries/scraper.rb
+++ lib/uq_libraries/scraper.rb
@@ -26,58 +26,13 @@ class UqLibraries::Scraper
     def self.scrape_details_page(library_url)
         details_page = Nokogiri::HTML(open(library_url))
 
-        library_name = details_page.css("h3")
-        test = details_page.css("table tr").collect do |level|
-            case level.css("a[href]").text.downcase
-                when "level 1"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                when "level 2"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                when "level 3"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                when "level 4"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                when "level 5"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                when "level 6"
-                    name = level.css("a[href]").text
-                    total_available = level.css(".right").text.split(" ")[0]
-                    out_of_available = level.css(".right").text.split(" ")[3]
-                    level = {name: name, total_available: total_available, out_of_available: out_of_available}
-                # else
-                #     puts "ERROR: #{level.css("a[href]").text.downcase}"
-                #     raise "There was an issue. Please submit an issue on GitHub: https://github.com/Tolenno/uq-library-cli-gem with a screenshot of the error".colorize(:red)
-            end
-        end.compact
+        test = details_page.css("table.chart tr").collect do |level|
+            right = level.css(".right").text.split
+            {name: level.css("a[href]").text, total_available: right[0], out_of_available: right[3]}
+        end
     end
 end
 
-# library_levels << {
-#     "#{library_name}" => {
-#         level_one: level_one_hash,
-#         level_two: level_two_hash,
-#         level_three: level_three_hash,
-#         level_four: level_four_hash, 
-#         level_five: level_five_hash,
-#         level_six: level_six_hash
-#     }
-# }
-
 # UqLibraries::Scraper.scrape_details_page("https://www.library.uq.edu.au/uqlsm/availablepcsembed.php?branch=BSL")
 
-# UqLibraries::Scraper.scrape_details_page(library_url)
+# UqLibraries::Scraper.scrape_details_page("https://www.library.uq.edu.au/uqlsm/availablepcsembed.php?branch=Duhig")
